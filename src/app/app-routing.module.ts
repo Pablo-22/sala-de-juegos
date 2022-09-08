de920321166b37e0d66b22734bcc2fcacbaff8ab
Pablo Córdoba <pablo.cordoba@bromus.com.ar>
2022-09-07 22:40:02 -0300

    Desarrollo del primer sprint - Home, login y quien soy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Vistas/home/home.component';
 import { JuegoComponent } from './Vistas/juego/juego.component';
+import { LoginComponent } from './Vistas/login/login.component';
 import { MenuComponent } from './Vistas/menu/menu.component';
+import { QuienSoyComponent } from './Vistas/quien-soy/quien-soy.component';
 
 const routes: Routes = [
-	{path: '', component: MenuComponent},
-	{path: 'juego', component: JuegoComponent}
+	{path: '', component: HomeComponent},
+	{path: 'juego', component: JuegoComponent},
+	{path: 'quien-soy', component: QuienSoyComponent},
+	{path: 'login', component: LoginComponent},
+	{path: 'menu', component: LoginComponent}
 ];
 
 @NgModule({
