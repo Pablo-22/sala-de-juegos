1baeaa06bcff908c007808dd5ce9ae3e077a8bb5
Pablo Córdoba <pablo.cordoba@bromus.com.ar>
2022-09-14 23:51:12 -0300

    Ajuste environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "AIzaSyBrFh_k6rpdcB2q7ZP1PjKyx6SU9-pjXKs",
+  authDomain: "labiv-2022.firebaseapp.com",
+  projectId: "labiv-2022",
+  storageBucket: "labiv-2022.appspot.com",
+  messagingSenderId: "736634794242",
+  appId: "1:736634794242:web:10a8fb77851b2a192b121b",
+  measurementId: "G-XW58REMNY0"
 };
