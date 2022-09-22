e441f5783c25fd4490c0ecf905fb2ae1d0b31212
Pablo Córdoba <pablo.cordoba@bromus.com.ar>
2022-09-22 18:59:03 -0300

    ajuste para deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,4 @@
 export const environment = {
-  firebase: {
-    projectId: 'labiv-2022',
-    appId: '1:736634794242:web:10a8fb77851b2a192b121b',
-    storageBucket: 'labiv-2022.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyBrFh_k6rpdcB2q7ZP1PjKyx6SU9-pjXKs',
-    authDomain: 'labiv-2022.firebaseapp.com',
-    messagingSenderId: '736634794242',
-    measurementId: 'G-XW58REMNY0',
-  },
   production: true,
   apiKey: "AIzaSyBrFh_k6rpdcB2q7ZP1PjKyx6SU9-pjXKs",
   authDomain: "labiv-2022.firebaseapp.com",
